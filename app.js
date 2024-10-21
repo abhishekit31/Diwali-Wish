81a3f90b346b04424a6522d2da7884fa94ab0671
Abhishek Kumar <abhishekit31@gmail.com>
2024-10-21 11:43:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function revelmessage(button){
+    let message = document.querySelector('.message');
+    message.innerText = "May in this Diwali, you be blessed with Good fortune, Wealth, Prosperity, and Happiness";
+    // Hide the button after revealing the message
+    button.style.display = 'none';
+    
+
+}
+
