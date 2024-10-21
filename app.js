21f7938638c5e73dc4a1870da2b8cf72e1bb60b8
Abhishek Kumar <abhishekit31@gmail.com>
2024-10-21 13:27:15 +0530

    adding this messages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function revelmessage(button){
     const message = document.querySelector('.message');
-    message.innerHTML = `Hope the festival of lights brings your bright sparles of peace, contentment joy, and happiness which stay with you throughout this year and also in this years to come. May the lamp of joy remain illuminated in your life now and forever <br><br><span class="regard">Regards,</span><br><span class="highlight">Abhi🙏</span>`;
+    message.innerHTML = `Hope the festival of lights brings your bright spakles of peace, contentment joy, and happiness which stay with you throughout this year and also in the years to come. May the lamp of joy remain illuminated in your life now and forever <br><br><span class="regard">Regards,</span><br><span class="highlight">Abhi🙏</span>`;
     // Hide the button after revealing the message
     button.style.display = 'none';
 }
